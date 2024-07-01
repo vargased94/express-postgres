ee12421da20860fc692849c1dcf34716dd088cdf
Eduardo Vargas <117415684+vargased94@users.noreply.github.com>
2024-07-01 10:26:21 -0600

    register user function

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -2,6 +2,20 @@ import { pool } from '../db.js';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcryptjs';
 
+export const register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const hashedPassword = bcrypt.hashSync(password);
+    const { rows } = await pool.query('INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *', [name, email, hashedPassword]);
+    if (rows.length === 0) {
+      return res.status(500).json({ message: 'An error occurred' });
+    }
+    res.status(201).json(rows[0]);
+  } catch (err) {
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+}
+
 export const login = async (req, res) => {
   const { email, password } = req.body;
   const { rows } = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
