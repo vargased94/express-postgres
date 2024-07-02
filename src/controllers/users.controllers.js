470b679adb75565ea3acd44da9ec04dd8afff61f
Eduardo Vargas <117415684+vargased94@users.noreply.github.com>
2024-07-02 11:08:43 -0600

    chore: Refactor SQL queries in users.controllers.js

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -1,13 +1,13 @@
 import { pool } from '../db.js';
 
 export const getUsers = async (req, res) => {
-  const { rows } = await pool.query('SELECT * FROM users');
+  const { rows } = await pool.query('SELECT id, name, email FROM users');
   res.json(rows);
 }
 
 export const getUser = async (req, res) => {
   const { id } = req.params;
-  const { rows } = await pool.query('SELECT * FROM users WHERE id = $1', [id]);
+  const { rows } = await pool.query('SELECT id, name, email FROM users WHERE id = $1', [id]);
   if (rows.length === 0) {
     return res.status(404).json({ message: 'User not found' });
   }
@@ -17,7 +17,7 @@ export const getUser = async (req, res) => {
 export const createUser = async (req, res) => {
   try {
     const { name, email } = req.body;
-    const { rows } = await pool.query('INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *', [name, email]);
+    const { rows } = await pool.query('INSERT INTO users (name, email) VALUES ($1, $2) RETURNING id, name, email', [name, email]);
     if (rows.length === 0) {
       return res.status(500).json({ message: 'An error occurred' });
     }
@@ -33,11 +33,11 @@ export const createUser = async (req, res) => {
 export const updateUser = async (req, res) => {
   const { id } = req.params;
   const { name, email } = req.body;
-  const { rows } = await pool.query('UPDATE users SET name = $1, email = $2 WHERE id = $3 RETURNING *', [name, email, id]);
-  if (rows.length === 0) {
+  const { rowCount } = await pool.query('UPDATE users SET name = $1, email = $2 WHERE id = $3 RETURNING id, name, email', [name, email, id]);
+  if (rowCount === 0) {
     return res.status(404).json({ message: 'User not found' });
   }
-  res.json(rows[0]);
+  res.sendStatus(204);
 };
 
 export const deleteUser = async (req, res) => {
