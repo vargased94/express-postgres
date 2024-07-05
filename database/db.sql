75dbf1f0f0e3c8c63a57b9c2a143fc6e78b79283
Eduardo Vargas <117415684+vargased94@users.noreply.github.com>
2024-07-05 15:22:51 -0600

    feat: Add customers table to database and customer CRUD endpoints

--- database/db.sql
+++ database/db.sql
@@ -6,10 +6,58 @@ CREATE TABLE users (
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE TABLE customers (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  phone VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO customers (name, phone, email) VALUES ('John Doe', '3333333333', 'john@gmail.com');
+INSERT INTO customers (name, phone, email) VALUES ('Sarah Doe', '4444444444', 'sarah@gmail.com');
+
 CREATE TABLE products (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
   price DECIMAL(10, 2) NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE orders (
+  id SERIAL PRIMARY KEY,
+  customer_id INT NOT NULL,
+  user_id INT NOT NULL,
+  FOREIGN KEY (customer_id) REFERENCES customers(id),
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE order_items (
+  id SERIAL PRIMARY KEY,
+  order_id INT NOT NULL,
+  product_id INT NOT NULL,
+  FOREIGN KEY (order_id) REFERENCES orders(id),
+  FOREIGN KEY (product_id) REFERENCES products(id),
+  quantity INT NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE invoices (
+  id SERIAL PRIMARY KEY,
+  customer_id INT NOT NULL,
+  user_id INT NOT NULL,
+  FOREIGN KEY (customer_id) REFERENCES customers(id),
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE invoice_orders (
+  id SERIAL PRIMARY KEY,
+  invoice_id INT NOT NULL,
+  order_id INT NOT NULL,
+  FOREIGN KEY (invoice_id) REFERENCES invoices(id),
+  FOREIGN KEY (order_id) REFERENCES orders(id),
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
